10ad79ed8228597dcada8120239c0cf59c7b8e40
Marvin H. Sielenkemper <sielenk@gmx.de>
2020-01-06 13:15:45 +0100

    implemented tic-tac-toe

--- src/sketch_trellis8x8_color_toggle.cpp
+++ src/sketch_trellis8x8_color_toggle.cpp
@@ -33,20 +33,116 @@ void setAll(uint32_t color)
   }
 }
 
-uint8_t colors[64] = {};
+uint8_t map(uint8_t xy)
+{
+  switch (xy)
+  {
+  case 0:
+  case 1:
+    return 0;
+  case 3:
+  case 4:
+    return 1;
+  case 6:
+  case 7:
+    return 2;
+  default:
+    return -1;
+  }
+}
+
+uint8_t unmap(uint8_t xy)
+{
+  return 3 * xy;
+}
+
+uint32_t playerColor(int8_t player, int8_t winner)
+{
+  switch (player)
+  {
+  case 1:
+    return (winner == 2) ? 0x0f0000 : 0x4f0000;
+  case 2:
+    return (winner == 1) ? 0x000f00 : 0x004f00;
+  default:
+    return 0x000000;
+  }
+}
+
+int8_t field[3][3] = {};
+int8_t player = 1;
+int8_t winner = -1;
 
 TrellisCallback onKey(keyEvent evt)
 {
-  uint8_t &color = colors[evt.bit.NUM];
-  color = (color + 1) & 7;
+  if (winner != -1)
+  {
+    bzero(field, sizeof(field));
+    player = 1;
+    winner = -1;
+  }
+  else
+  {
+    auto const xx = map(evt.bit.NUM & 7);
+    auto const yy = map((evt.bit.NUM >> 3) & 7);
+    auto &cell = field[xx][yy];
 
-  uint32_t const pixelColor =
-      (((color & 1) != 0) * 0x4f0000) |
-      (((color & 2) != 0) * 0x004f00) |
-      (((color & 4) != 0) * 0x00004f) |
-      0;
+    if (xx != -1 && yy != -1 && !cell)
+    {
+      cell = player;
 
-  trellis.setPixelColor(evt.bit.NUM, pixelColor);
+      uint8_t rp[] = {1, 1, 1};
+      uint8_t cp[] = {1, 1, 1};
+      uint8_t dp0 = 1;
+      uint8_t dp1 = 1;
+      for (uint8_t u = 0; u < 3; ++u)
+      {
+        dp0 *= field[u][u];
+        dp1 *= field[u][2 - u];
+
+        for (uint8_t v = 0; v < 3; ++v)
+        {
+          rp[u] *= field[u][v];
+          cp[u] *= field[v][u];
+        }
+      }
+
+      if (rp[0] && rp[1] && rp[2])
+      {
+        // draw
+        winner = 0;
+      }
+      else if (rp[0] == 1 || rp[1] == 1 || rp[2] == 1 || cp[0] == 1 || cp[1] == 1 || cp[2] == 1 || dp0 == 1 || dp1 == 1)
+      {
+        // win player 1
+        winner = 1;
+      }
+      else if (rp[0] == 8 || rp[1] == 8 || rp[2] == 8 || cp[0] == 8 || cp[1] == 8 || cp[2] == 8 || dp0 == 8 || dp1 == 8)
+      {
+        // win player 2
+        winner = 2;
+      }
+      else
+      {
+        player ^= 3;
+      }
+    }
+  }
+
+  for (uint8_t xx = 0; xx < 3; ++xx)
+  {
+    for (uint8_t yy = 0; yy < 3; ++yy)
+    {
+      auto const pixelColor = playerColor(field[xx][yy], winner);
+      auto const x = unmap(xx);
+      auto const y = unmap(yy);
+
+      trellis.setPixelColor(x, y, pixelColor);
+      trellis.setPixelColor(x + 1, y, pixelColor);
+      trellis.setPixelColor(x, y + 1, pixelColor);
+      trellis.setPixelColor(x + 1, y + 1, pixelColor);
+    }
+  }
   trellis.show();
 
   return nullptr;
@@ -63,23 +159,27 @@ void handlerRoot()
   String buffer;
 
   buffer += "<!DOCTYPE html><html><head><meta http-equiv='Content-Type' content='text/html; charset=utf-8'></head><body><table border='1'>";
-  for (uint8_t y = 0; y < 8; ++y)
+  for (uint8_t y = 0; y < 3; ++y)
   {
     buffer += "<tr>";
-    for (uint8_t x = 0; x < 8; ++x)
+    for (uint8_t x = 0; x < 3; ++x)
     {
-      auto const i((y << 3) | x);
-      auto const c(colors[i]);
+      auto const c(field[x][y]);
 
-      buffer += "<td style='background-color: #";
-      buffer += (c & 1) ? "ff" : "00";
-      buffer += (c & 2) ? "ff" : "00";
-      buffer += (c & 4) ? "ff" : "00";
-      buffer += ";'>";
-      buffer += i;
-      buffer += " ";
-      buffer += static_cast<int>(c);
-      buffer += "</td>";
+      buffer += "<td style='width:20px; height:20px; background-color: ";
+      switch (c)
+      {
+      case 1:
+        buffer += "red";
+        break;
+      case 2:
+        buffer += "green";
+        break;
+      default:
+        buffer += "black";
+        break;
+      }
+      buffer += ";'/>";
     }
     buffer += "</tr>";
   }
@@ -128,6 +228,15 @@ void setup()
   setAll(0x004f00);
   setAll(0x4f0000);
   setAll(0x000000);
+
+  for (int i = 0; i < 8; ++i)
+  {
+    trellis.setPixelColor(i, 2, 0x4f4f4f);
+    trellis.setPixelColor(i, 5, 0x4f4f4f);
+    trellis.setPixelColor(2, i, 0x4f4f4f);
+    trellis.setPixelColor(5, i, 0x4f4f4f);
+  }
+  trellis.show();
 
   for (uint8_t i = 0; i < 64; ++i)
   {
