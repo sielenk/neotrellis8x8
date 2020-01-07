6eb1e8d65645ea857e66f2bfef552d6ecbf12ffd
Marvin H. Sielenkemper <sielenk@gmx.de>
2020-01-07 13:21:36 +0100

    extracted the game into a class

--- src/main.cpp
+++ src/main.cpp
@@ -33,62 +33,98 @@ void setAll(uint32_t color)
   }
 }
 
-uint8_t map(uint8_t xy)
+class Game
 {
-  switch (xy)
+  static uint8_t map(uint8_t xy)
   {
-  case 0:
-  case 1:
-    return 0;
-  case 3:
-  case 4:
-    return 1;
-  case 6:
-  case 7:
-    return 2;
-  default:
-    return -1;
+    switch (xy)
+    {
+    case 0:
+    case 1:
+      return 0;
+    case 3:
+    case 4:
+      return 1;
+    case 6:
+    case 7:
+      return 2;
+    default:
+      return UINT8_MAX;
+    }
   }
-}
 
-uint8_t unmap(uint8_t xy)
-{
-  return 3 * xy;
-}
-
-uint32_t playerColor(int8_t player, int8_t winner)
-{
-  switch (player)
+  enum CellState : uint8_t
   {
-  case 1:
-    return (winner == 2) ? 0x0f0000 : 0x4f0000;
-  case 2:
-    return (winner == 1) ? 0x000f00 : 0x004f00;
-  default:
-    return 0x000000;
-  }
-}
+    EMPTY,
+    PLAYER1,
+    PLAYER2,
+    GRID = UINT8_MAX
+  };
 
-int8_t field[3][3] = {};
-int8_t player = 1;
-int8_t winner = -1;
+  CellState field[3][3];
+  CellState player;
+  CellState winner;
 
-TrellisCallback onKey(keyEvent evt)
-{
-  if (winner != -1)
+  void init()
   {
     bzero(field, sizeof(field));
-    player = 1;
-    winner = -1;
+    player = PLAYER1;
+    winner = EMPTY;
   }
-  else
-  {
-    auto const xx = map(evt.bit.NUM & 7);
-    auto const yy = map((evt.bit.NUM >> 3) & 7);
-    auto &cell = field[xx][yy];
 
-    if (xx != -1 && yy != -1 && !cell)
+  CellState *cell(uint8_t x, uint8_t y)
+  {
+    auto const xx = map(x);
+    auto const yy = map(y);
+
+    if (xx == UINT8_MAX || yy == UINT8_MAX)
     {
+      return nullptr;
+    }
+
+    return &field[xx][yy];
+  }
+
+  CellState const *cell(uint8_t x, uint8_t y) const
+  {
+    return const_cast<Game *>(this)->cell(x, y);
+  }
+
+  CellState cellState(uint8_t x, uint8_t y) const
+  {
+    if (CellState const *cellPtr = cell(x, y))
+    {
+      return *cellPtr;
+    }
+    else
+    {
+      return GRID;
+    }
+  }
+
+public:
+  Game()
+  {
+    init();
+  }
+
+  bool onKey(uint8_t x, uint8_t y, bool rising)
+  {
+    if (winner != EMPTY)
+    {
+      init();
+    }
+    else
+    {
+      CellState *const cellPtr = cell(x, y);
+
+      if (!cellPtr || *cellPtr != EMPTY)
+      {
+        return false;
+      }
+
+      auto &cell = *cellPtr;
+
       cell = player;
 
       uint8_t rp[] = {1, 1, 1};
@@ -110,40 +146,95 @@ TrellisCallback onKey(keyEvent evt)
       if (rp[0] == 1 || rp[1] == 1 || rp[2] == 1 || cp[0] == 1 || cp[1] == 1 || cp[2] == 1 || dp0 == 1 || dp1 == 1)
       {
         // win player 1
-        winner = 1;
+        winner = PLAYER1;
       }
       else if (rp[0] == 8 || rp[1] == 8 || rp[2] == 8 || cp[0] == 8 || cp[1] == 8 || cp[2] == 8 || dp0 == 8 || dp1 == 8)
       {
         // win player 2
-        winner = 2;
+        winner = PLAYER2;
       }
       else if (rp[0] && rp[1] && rp[2])
       {
         // draw
-        winner = 0;
+        winner = GRID;
       }
       else
       {
-        player ^= 3;
+        player = (player == PLAYER1) ? PLAYER2 : PLAYER1;
       }
     }
+
+    return true;
   }
 
-  for (uint8_t xx = 0; xx < 3; ++xx)
+  uint32_t cellColor(uint8_t x, uint8_t y) const
   {
-    for (uint8_t yy = 0; yy < 3; ++yy)
+    switch (cellState(x, y))
     {
-      auto const pixelColor = playerColor(field[xx][yy], winner);
-      auto const x = unmap(xx);
-      auto const y = unmap(yy);
-
-      trellis.setPixelColor(x, y, pixelColor);
-      trellis.setPixelColor(x + 1, y, pixelColor);
-      trellis.setPixelColor(x, y + 1, pixelColor);
-      trellis.setPixelColor(x + 1, y + 1, pixelColor);
+    case PLAYER1:
+      return (winner == PLAYER2) ? 0x0f0000 : 0x3f0000;
+    case PLAYER2:
+      return (winner == PLAYER1) ? 0x000f00 : 0x003f00;
+    case GRID:
+      return (winner != EMPTY) ? 0x3f3f3f : (player == PLAYER1) ? 0x3f2a2a : 0x2a3f2a;
+    default:
+      return 0x000000;
     }
   }
+
+  String
+  cellColorHtml(uint8_t x, uint8_t y) const
+  {
+    switch (cellState(x, y))
+    {
+    case PLAYER1:
+      return (winner == PLAYER2) ? "darkred" : "red";
+    case PLAYER2:
+      return (winner == PLAYER1) ? "green" : "lightgreen";
+    case GRID:
+      return (winner != EMPTY) ? "white" : (player == PLAYER1) ? "#ffe0e0" : "#e0ffe0";
+    default:
+      return "black";
+    }
+  }
+};
+
+Game game;
+
+void showGame()
+{
+  for (uint8_t x = 0; x < 8; ++x)
+  {
+    for (uint8_t y = 0; y < 8; ++y)
+    {
+      auto const pixelColor = game.cellColor(x, y);
+
+      trellis.setPixelColor(x, y, pixelColor);
+    }
+  }
+
   trellis.show();
+}
+
+TrellisCallback onKey(keyEvent evt)
+{
+  bool isRising;
+  switch (evt.bit.EDGE)
+  {
+  case SEESAW_KEYPAD_EDGE_FALLING:
+    isRising = false;
+    break;
+  case SEESAW_KEYPAD_EDGE_RISING:
+    isRising = false;
+    break;
+  default:
+    return nullptr;
+  }
+
+  if (game.onKey(evt.bit.NUM & 7, (evt.bit.NUM >> 3) & 7, isRising))
+  {
+    showGame();
+  }
 
   return nullptr;
 }
@@ -159,26 +250,13 @@ void handlerRoot()
   String buffer;
 
   buffer += "<!DOCTYPE html><html><head><meta http-equiv='Content-Type' content='text/html; charset=utf-8'></head><body><table border='1'>";
-  for (uint8_t y = 0; y < 3; ++y)
+  for (uint8_t y = 0; y < 8; ++y)
   {
     buffer += "<tr>";
-    for (uint8_t x = 0; x < 3; ++x)
+    for (uint8_t x = 0; x < 8; ++x)
     {
-      auto const c(field[x][y]);
-
       buffer += "<td style='width:20px; height:20px; background-color: ";
-      switch (c)
-      {
-      case 1:
-        buffer += "red";
-        break;
-      case 2:
-        buffer += "green";
-        break;
-      default:
-        buffer += "black";
-        break;
-      }
+      buffer += game.cellColorHtml(x, y);
       buffer += ";'/>";
     }
     buffer += "</tr>";
@@ -196,8 +274,6 @@ void handlerIcon()
   auto const size = file.size();
   auto const buffer = std::unique_ptr<char>{new char[size]};
 
-  Serial.printf("size = %d\r\n", size);
-
   file.readBytes(buffer.get(), size);
   server.send(200, "image/x-icon", buffer.get(), size);
 
@@ -209,26 +285,26 @@ void setup()
   Serial.begin(74880);
   Serial.println("setup - start");
 
-  wifiManager.autoConnect();
-
   pinMode(INT_PIN, INPUT);
 
   Serial.printf("trellis: %s\r\n", trellis.begin() ? "succeeded" : "failed");
-  Serial.printf("SPIFFS: %s\r\n", SPIFFS.begin() ? "succeeded" : "failed");
 
   setAll(0x00004f);
-  setAll(0x004f00);
-  setAll(0x4f0000);
-  setAll(0x000000);
 
-  for (int i = 0; i < 8; ++i)
-  {
-    trellis.setPixelColor(i, 2, 0x4f4f4f);
-    trellis.setPixelColor(i, 5, 0x4f4f4f);
-    trellis.setPixelColor(2, i, 0x4f4f4f);
-    trellis.setPixelColor(5, i, 0x4f4f4f);
-  }
-  trellis.show();
+  wifiManager.autoConnect();
+
+  setAll(0x004f00);
+
+  Serial.printf("SPIFFS: %s\r\n", SPIFFS.begin() ? "succeeded" : "failed");
+
+  setAll(0x4f0000);
+
+  server.on("/", HTTP_GET, handlerRoot);
+  server.on("/favicon.ico", HTTP_GET, handlerIcon);
+  server.begin();
+  Serial.println("HTTP server started");
+
+  setAll(0x000000);
 
   for (uint8_t i = 0; i < 64; ++i)
   {
@@ -236,12 +312,9 @@ void setup()
     trellis.registerCallback(i, &onKey);
   }
 
-  server.on("/", HTTP_GET, handlerRoot);
-  server.on("/favicon.ico", HTTP_GET, handlerIcon);
-  server.begin();
-  Serial.println("HTTP server started");
-
   Serial.println("setup - end");
+
+  showGame();
 }
 
 unsigned long secondsOld;
